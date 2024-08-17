557740c33bad6ed5f2bb59bc3a7d61a52d4c61ed
rhea-so <jeonghyeon.rhea@gmail.com>
2024-08-17 20:07:38 +0900

    add license

--- npmjs-publish.sh
+++ npmjs-publish.sh
@@ -1,8 +1,9 @@
 mkdir temp
 
 cp ./package.json ./temp/package.json
-cp ./README.md ./temp/README.md
 cp ./index.js ./temp/index.js
+cp ./README.md ./temp/README.md
+cp ./LICENSE ./temp/LICENSE
 
 npm publish ./temp
 
